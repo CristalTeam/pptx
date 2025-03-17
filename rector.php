42f227d59d3a94ac570e77b572cc49c682ab1189
Marvin Durot <marvin.durot@koesio.com>
2025-03-17 13:50:38 +0100

    Upgrade to PHP 8.1 with Rector

--- rector.php
+++ rector.php
@@ -9,7 +9,7 @@ return RectorConfig::configure()
         __DIR__ . '/Presentation',
         __DIR__ . '/tests',
     ])
-    ->withPhpSets(php80: true)
+    ->withPhpSets(php81: true)
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0);
