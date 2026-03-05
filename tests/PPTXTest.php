44f65f79c1d1e8467e421ce41ff7765c716e8b61
Jérémy TREDANT <jeremy.tredant@koesio.com>
2026-03-05 16:32:44 +0100

    menage

--- tests/PPTXTest.php
+++ tests/PPTXTest.php
@@ -38,22 +38,9 @@ class PPTXTest extends TestCase
     public function it_merges_two_pptx(): void
     {
         $pptxs = [
+            'garde',
             'FIN',
-            'T5',
             'MILIEU',
-            'C1',
-            'C2',
-            'C3',
-            'DEVM',
-            'C4',
-            'C5',
-            'aquitaine',
-            'pc',
-            'DEVM2',
-            'T1',
-            'T9',
-            'DEV',
-            'DELL',
         ];
 
 
